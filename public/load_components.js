b74d262a71eff21a076bd15b23f6b1a2b4c28bd1
Sandip <sandipvaghela311@gmail.com>
2019-03-08 01:09:24 +0530

    init repo

---
+++ public/load_components.js
@@ -0,0 +1,9 @@
+(adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-6628325093439880",
+    enable_page_level_ads: true
+});
+
+/* const buttons = document.querySelectorAll('.mdc-button');
+for (const button of buttons) {
+    mdc.ripple.MDCRipple.attachTo(button);
+} */
