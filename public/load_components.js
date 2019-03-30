4c1355d202e0932558444e5d7b79f905a5289024
Sandip <sandipvaghela311@gmail.com>
2019-03-30 13:00:07 +0530

    Many Changes
    Change background
    prepare for testing MDC Components
    
    Co-Authored-By: janvi-parmar <janvi-parmar@users.noreply.github.com>

--- public/load_components.js
+++ public/load_components.js
@@ -3,14 +3,13 @@
     enable_page_level_ads: true
 });
 
-/* const buttons = document.querySelectorAll('.mdc-button');
 window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
 
-    gtag('config', 'UA-133685173-2');
+gtag('config', 'UA-133685173-2');
 
 const buttons = document.querySelectorAll('.mdc-button');
 for (const button of buttons) {
     mdc.ripple.MDCRipple.attachTo(button);
-} */
+}
