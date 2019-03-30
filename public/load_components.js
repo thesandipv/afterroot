3b539754ea9343335155c33b8444f49545278631
Sandip <sandipvaghela311@gmail.com>
2019-03-30 12:58:28 +0530

    Moved analytics code to js file

--- public/load_components.js
+++ public/load_components.js
@@ -4,6 +4,13 @@
 });
 
 /* const buttons = document.querySelectorAll('.mdc-button');
+window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-133685173-2');
+
+const buttons = document.querySelectorAll('.mdc-button');
 for (const button of buttons) {
     mdc.ripple.MDCRipple.attachTo(button);
 } */
