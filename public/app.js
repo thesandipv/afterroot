854089b121152b72ea6f4bfe3f7166d0c6e08ce3
Sandip <sandipvaghela311@gmail.com>
2019-07-10 22:35:54 +0530

    moved google ads script

--- public/app.js
+++ public/app.js
@@ -1,10 +1,5 @@
 import { MDCRipple } from '@material/ripple/index';
 
-(adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-6628325093439880",
-    enable_page_level_ads: true
-});
-
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
@@ -13,5 +8,3 @@ gtag('config', 'UA-133685173-2');
 
 const buttons = [].map.call(document.querySelectorAll('.mdc-button'), function(el) {
     return new MDCRipple.attachTo(el);
-});
-
