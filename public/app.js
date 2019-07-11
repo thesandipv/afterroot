f8ed1a7e7f2fe5ac496e2e67a86881398b2c12c5
Sandip <sandipvaghela311@gmail.com>
2019-07-11 12:38:20 +0530

    added bootstrap via npm

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
 import { MDCRipple } from '@material/ripple/';
+import { MDCTopAppBar } from '@material/top-app-bar';
+import 'bootstrap';
 
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
@@ -6,6 +8,9 @@ gtag('js', new Date());
 
 gtag('config', 'UA-133685173-2');
 
-const buttons = [].map.call(document.querySelectorAll('.mdc-button'), function(el) {
+const buttons = [].map.call(document.querySelectorAll('.mdc-button'), function (el) {
     return new MDCRipple.attachTo(el);
 });
+
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
