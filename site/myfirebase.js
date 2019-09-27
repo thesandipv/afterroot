3a897e4f4877e8906480cf90f79f8be078f0ac71
Sandip <sandipvaghela311@gmail.com>
2019-09-27 08:38:41 +0530

    update footer includes and code changes

---
+++ site/myfirebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALjnvK24GNHeRXsU_XGVgzPQLgVua39Xw",
+    authDomain: "afterhashroot.firebaseapp.com",
+    databaseURL: "https://afterhashroot.firebaseio.com",
+    projectId: "afterhashroot",
+    storageBucket: "afterhashroot.appspot.com",
+    messagingSenderId: "506969783274",
+    appId: "1:506969783274:web:f5b8757df611b355eb9093"
+};
+
+var project = firebase.initializeApp(firebaseConfig);
