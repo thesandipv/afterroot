492109e760bdd8c1defa82b70f797a9e18c7ac9d
Sandip <sandipvaghela311@gmail.com>
2019-12-15 16:45:34 +0530

    dist moved to src

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,14 +1,29 @@
 const autoprefixer = require('autoprefixer');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = [{
+  mode: 'development',
   entry: ['./src/app.scss', './src/app.js', './src/include.js'],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "AfterROOT - Everything's connected."
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
       {
         test: /\.scss$/,
         use: [
@@ -30,7 +45,8 @@ module.exports = [{
             loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: ['./node_modules']
+                includePaths: ['./node_modules'],
+                implementation: require('sass'),
               }
             }
           }
