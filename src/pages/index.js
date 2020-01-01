d4d443e870c46e1044154b60de753ebfcb86fdb0
Sandip <sandipvaghela311@gmail.com>
2020-01-01 13:53:16 +0530

    DEV: init v0.1.3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Header from "../components/header"
+import config from "../../data/SiteConfig"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Header headerText={config.siteTitle} />
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <Link to="/contact/">Contact</Link>
+  </div>
+)
