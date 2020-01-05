7bc4bee397987ac6429f7b2ab79c1652e76ac002
Sandip <sandipvaghela311@gmail.com>
2020-01-05 13:45:01 +0530

    DEV: Added Material Components

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,24 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const config = require("./data/SiteConfig")
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: config.siteTitle,
+    url: config.siteUrl,
+    description: config.siteDescription,
+    twitterUsername: config.userTwitter,
+  },
+
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: ["./node_modules"],
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 }
