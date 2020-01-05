da02bbcd7be0ce1ae8fff1d86803b15d217276df
Sandip <sandipvaghela311@gmail.com>
2020-01-05 14:16:40 +0530

    Chnaged all comp names to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-import Header from "../components/Header/header"
+import Header from "../components/Header"
 import config from "../../data/SiteConfig"
 import { Link } from "gatsby"
 
 export default () => (
-  <div style={{ color: `purple` }}>
+  <>
     <Header title={config.siteTitle} />
-    <p>What a world.</p>
-    <Link to="/contact/">Contact</Link>
-  </div>
+  </>
 )
