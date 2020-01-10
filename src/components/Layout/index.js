21771b9edd412f7283c15ab5c3556bec7aa9817a
Sandip <sandipvaghela311@gmail.com>
2020-01-10 21:10:06 +0530

    DEV: added footer

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Header from "../Header"
-import config from "../../../data/SiteConfig"
+import Footer from "../Footer"
 
-export default ({ children }) => (
+export default ({ children, title }) => (
   <>
-    <Header title={config.siteTitle}></Header>
+    <Header title={title}></Header>
     <div>{children}</div>
+    <Footer />
   </>
 )
