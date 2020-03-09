daf569136264638d9336b9fb302a10858d16dad9
Sandip <sandipvaghela311@gmail.com>
2020-03-09 23:03:50 +0530

    build date in footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-build-date`,
+      options: {
+        formatAsDateString: true, // boolean, defaults to true - if false API will return unformatted string from new Date()
+        formatting: {
+          format: "DD-MM-YYYY HH:MM:SS", // string, defaults to "MM/DD/YYYY" - pass in any acceptable date-and-time format
+          utc: false, // boolean, defaults to false - output time as UTC or not, following date-and-time API
+        },
+      },
+    },
   ],
 }
