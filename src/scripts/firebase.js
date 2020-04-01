79a0109ddd35e4e72034478ac2c26ddc4e60b5b9
Sandip <sandipvaghela311@gmail.com>
2020-04-01 12:43:27 +0530

    added list item logos

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/database"
+import "firebase/analytics"
 export const firebaseConfig = {
   apiKey: "AIzaSyCVgZRrAD3bWNq-wMsx0DA08BbbOaTk-qg",
   authDomain: "afterroot.firebaseapp.com",
@@ -10,9 +11,16 @@ export const firebaseConfig = {
   appId: "1:910276298420:web:1fd8216cb2422070f979e1",
   measurementId: "G-LJ042LZHR5",
 }
-var app = firebase.initializeApp(firebaseConfig)
-
-export default firebase
-export function getDatabase() {
-  return app.database()
+var app
+export function initFirebase() {
+  if (app == null) {
+    console.log("Initializing Firebase app")
+    app = firebase.initializeApp(firebaseConfig)
+  }
+  console.log("Firebase app already initialized")
+  return app
 }
+export function getDatabase() {
+  return initFirebase().database()
+}
+export default firebase
