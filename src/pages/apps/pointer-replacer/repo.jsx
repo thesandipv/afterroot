e3db9841468305be6df1581c45300d1f2032403b
Sandip <sandipvaghela311@gmail.com>
2020-04-20 00:56:06 +0530

    fix firebase auto

--- src/pages/apps/pointer-replacer/repo.jsx
+++ src/pages/apps/pointer-replacer/repo.jsx
@@ -3,24 +3,30 @@ import Layout from "../../../components/Layout"
 import Container from "../../../components/Container/container"
 import * as firebaseui from "firebaseui"
 import firebase, { initFirebase } from "../../../scripts/firebase"
+import "../../../../node_modules/firebaseui/dist/firebaseui.css"
 
 class Repo extends Component {
   componentDidMount() {
     var uiConfig = {
-      signInSuccessUrl: "<url-to-redirect-to-on-success>",
+      callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+          return true
+        },
+        uiShown: function() {
+          document.getElementById("loader").style.display = "none"
+        },
+      },
       signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        {
+          provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          customParameters: {
+            prompt: "select_account",
+          },
+        },
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
       ],
-      // tosUrl and privacyPolicyUrl accept either url string or a callback
-      // function.
-      // Terms of service url/callback.
       tosUrl: "/privacy-policy",
-      // Privacy policy url/callback.
-      privacyPolicyUrl: function() {
-        window.location.assign("/privacy-policy")
-      },
+      privacyPolicyUrl: "/privacy-policy",
     }
 
     var ui = new firebaseui.auth.AuthUI(initFirebase().auth())
@@ -33,6 +39,9 @@ class Repo extends Component {
         <Layout title="Pointer Repository" navIcon="arrow_back">
           <Container styles={{ textAlign: "center" }}></Container>
           <div id="fb_ui"></div>
+          <div id="loader" style={{ textAlign: "center" }}>
+            Loading...
+          </div>
         </Layout>
       </>
     )
