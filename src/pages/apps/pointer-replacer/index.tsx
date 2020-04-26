29ed3be2d4098ac42d2d678fa52362e50c0fcbc3
Sandip <sandipvaghela311@gmail.com>
2020-04-26 20:50:09 +0530

    removed react-youtube and replaced with iframe

--- src/pages/apps/pointer-replacer/index.tsx
+++ src/pages/apps/pointer-replacer/index.tsx
@@ -1,19 +1,33 @@
 import React from "react"
 import Layout from "src/components/Layout"
-import YouTube, { Options } from "react-youtube"
 import Container from "src/components/Container/container"
-
-const options: Options = {
-  playerVars: {
-    autoplay: 0,
-  },
-}
 
 export default () => (
   <>
     <Layout title="Pointer Replacer" navIcon="arrow_back">
-      <Container styles={{ textAlign: "center", alignContent: "center" }}>
-        <YouTube id="player" videoId="eWcV0PUwlzs" opts={options} />
+      <Container
+        styles={{
+          position: "relative",
+          overflow: "hidden",
+          paddingTop: "56.25%",
+          width: "100%",
+        }}
+      >
+        <iframe
+          src="https://www.youtube.com/embed/eWcV0PUwlzs"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        />
       </Container>
     </Layout>
   </>
