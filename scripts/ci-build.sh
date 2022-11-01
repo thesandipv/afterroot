8fff61bf6f271ec40739a042341b15a4166ab098
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2022-11-01 20:31:26 +0530

    update build step

---
+++ scripts/ci-build.sh
@@ -0,0 +1,10 @@
+chmod +x ./scripts/build.sh
+chmod +x ./blog/scripts/moveBlog.sh
+echo Run npm install on root
+npm install
+cd ./blog
+echo Run npm install on blog
+npm install
+cd ..
+./scripts/build.sh
+npm run export
