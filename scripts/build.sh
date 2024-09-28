fa98e61c5638a80c86d966ac6188228255899155
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2024-09-28 12:53:49 +0530

    Update to Next-14 (#567)
    * update dependencies
    
    * update config and packages
    
    * update to next 14

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,13 @@
-npm run build
+mkdir ./public/ar/
+
+echo ">> afterroot: Building blog"
 cd blog
 npm run build
+
 cd ..
-./blog/scripts/moveBlog.sh
+
+echo ">> afterroot: Moving blog"
+mv ./blog/build/* ./public/ar/
+
+echo ">> afterroot: Building afterroot"
+npm run build
