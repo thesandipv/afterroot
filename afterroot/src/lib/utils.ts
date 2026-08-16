514212afab9d5af25876770f15ac211ed9b4792a
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2026-08-16 19:33:26 +0530

    add eslint tasks for linting and fixing in VSCode

--- afterroot/src/lib/utils.ts
+++ afterroot/src/lib/utils.ts
@@ -3,6 +3,7 @@ import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  // eslint-disable-next-line tailwindcss/no-custom-classname
   return twMerge(clsx(inputs))
 }
 
