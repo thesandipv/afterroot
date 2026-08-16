40f3439f2395717560a451f543d0ca188bcf2a85
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2026-08-16 14:00:43 +0530

    init shadn-svelte

---
+++ afterroot/src/lib/components/ui/button/index.ts
@@ -0,0 +1,16 @@
+import type { ButtonProps, ButtonSize, ButtonVariant } from "./button.svelte"
+import Root, {
+
+  buttonVariants,
+} from "./button.svelte"
+
+export {
+  //
+  Root as Button,
+  type ButtonProps,
+  type ButtonSize,
+  type ButtonVariant,
+  buttonVariants,
+  type ButtonProps as Props,
+  Root,
+}
